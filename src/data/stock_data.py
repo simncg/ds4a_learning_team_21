6ce680ee548e103e1286cb55e7fd7f505bc44b75
simncg <simncg@gmail.com>
2023-06-28 01:13:57 -0400

    Now paths are absolute and not relative

--- src/data/stock_data.py
+++ src/data/stock_data.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
@@ -6,11 +7,12 @@ import json
 import datetime
 from ..utils.date_json_handler import date_handler
 
+DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "data")
 
 class StockData:
     """
     Class to scrape and retrieve stock data and related financial data of a company.
-    
+
     Attributes
     ----------
     symbol : str
@@ -34,13 +36,13 @@ class StockData:
             The stock symbol of the company.
         """
         self.symbol = symbol
-        self.urls = self._create_urls()
-        self.company_data = self._create_dict_company()
+        self.urls = self.__create_urls()
+        self.company_data = self.__create_dict_company()
         self.scrape_done = False
         self.retrieve_done = False
 
 
-    def _create_urls(self):
+    def __create_urls(self):
         """
         Private method to construct the URLs for various financial statements.
         """
@@ -61,7 +63,7 @@ class StockData:
         
         return urls
 
-    def _create_dict_company(self):
+    def __create_dict_company(self):
         """
         Private method to initialize a dictionary for storing all the company's data.
         """
@@ -76,8 +78,7 @@ class StockData:
         
         return all_data_dict
 
-
-    def _process_key(self, key):
+    def __process_key(self, key):
         """
         Private method to process the key of a URL.
 
@@ -97,22 +98,12 @@ class StockData:
             processed_key = key
         return processed_key, frequency
 
-    def scrape_financial_statements(self):
-        """
-        Method to scrape financial statements for the company.
-        """
-        self.company_data = self._scrape_stock_analysis()
-        self.scrape_done = True
-
-    
-    def _scrape_stock_analysis(self):
+    def __scrape_stock_analysis(self):
         """
         Private method to scrape the financial data of the company from stockanalysis.com.
         """
-
         for key in self.urls.keys():
-
-            processed_key, frequency = self._process_key(key)
+            processed_key, frequency = self.__process_key(key)
 
             try:
                 page = requests.get(self.urls[key])
@@ -124,59 +115,32 @@ class StockData:
                     requests.exceptions.RequestException) as err:
 
                 print (f"Error for {self.symbol}:", err)
-
-
                 continue
 
             try:
                 soup = BeautifulSoup(page.content, "html.parser")
                 table = soup.find('table')
 
-                # If data is not found continue to next element in the loop 
                 if table is None:
                     continue
-                # Else format data and save it 
                 else: 
-                    # Obtain colnames (which correspond to dates)
                     colnames = table.find_all('th')
-
-                    # Cleaned column names (dates)
                     colnames = [date.text.strip() for date in colnames]
-
-                    # Get the values of the different metrics
                     rows = table.find_all('tr')
-
-                    # list to hold all table data
                     data = []
-
-                    # iterate over each row
                     for row in rows:
-
-                        # find all columns in the row
                         cols = row.find_all('td')
-
-                        # get the text from the columns
                         cols = [col.text.strip() for col in cols]
-
-                        # add the columns to the data array
                         data.append(cols)
 
-                    # create a pandas dataframe from the data
                     df = pd.DataFrame(data)
-
-                    # assign column names
                     df.columns = colnames
-
-                    # rename Column name from Quarter Ended to Metrics
                     df = df.rename(columns={"Quarter Ended": "Metrics"})
-
-                    # drop empty first row
                     df = df.drop(0)
 
-                    # Save file in CSV (Should be run from pipeline folder)
-                    df.to_csv(f"../data/raw/financial_statements/{processed_key}/{frequency}/{self.symbol}.csv", index = False)
+                    file_path = os.path.join(DATA_PATH, "raw", "financial_statements", processed_key, frequency, f"{self.symbol}.csv")
+                    df.to_csv(file_path, index = False)
 
-                    # Save info in dictionary
                     self.company_data[processed_key][frequency] = df.to_dict()
 
             except AttributeError as e:
@@ -185,14 +149,17 @@ class StockData:
 
             except Exception as e:
                 print(f"An unexpected error occurred for {self.symbol}:", e)
-
                 continue
 
         return self.company_data
-
-
     
-    
+    def scrape_financial_statements(self):
+        """
+        Method to scrape financial statements for the company.
+        """
+        self.company_data = self.__scrape_stock_analysis()
+        self.scrape_done = True
+
     def retrieve_profile_stock_price(self, start, end):
         """
         Method to retrieve stock price data and company profile from Yahoo Finance.
@@ -205,20 +172,15 @@ class StockData:
             The end date for the stock price data retrieval in the format 'YYYY-MM-DD'.
         """
         try:
-            # Retrieve data from Yahoo Finance
             sym = Ticker(self.symbol)
-            
-            # Obtain stock data
             stock_data = sym.history(start=start, end=end)
-            
-            # Get company profile 
             profile = sym.asset_profile[self.symbol]
-            
-            # Save CSV file 
-            stock_data.reset_index().to_csv(f"../data/raw/stock_prices/{self.symbol}.csv", index = False)
-            
-            # Save company profile as JSON
-            with open(f"../data/raw/company_profile/{self.symbol}.json", 'w') as f:
+
+            stock_price_path = os.path.join(DATA_PATH, "raw", "stock_prices", f"{self.symbol}.csv")
+            stock_data.reset_index().to_csv(stock_price_path, index = False)
+
+            profile_path = os.path.join(DATA_PATH, "raw", "company_profile", f"{self.symbol}.json")
+            with open(profile_path, 'w') as f:
                 json.dump(profile, f)
 
             self.company_data["stock_price"] = stock_data.reset_index().to_dict()
@@ -229,13 +191,13 @@ class StockData:
             
         self.retrieve_done = True
         
-        
     def save_to_json(self):
         """
         Method to save the scraped and retrieved company data in JSON format.
         """
         if self.scrape_done and self.retrieve_done:
-            with open(f"../data/raw/json_data/{self.symbol}.json", 'w') as f:
+            json_path = os.path.join(DATA_PATH, "raw", "json_data", f"{self.symbol}.json")
+            with open(json_path, 'w') as f:
                 json.dump(self.company_data, f, default=date_handler)
         else:
             print("Scrape or retrieve_profile_stock_price methods have not been executed yet.")
